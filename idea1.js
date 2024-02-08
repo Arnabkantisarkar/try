3a4b669d252c09442e6c98136fc7074b2fc148b1
Arnab Kanti Sarkar <141022364+Arnabkantisarkar@users.noreply.github.com>
2024-02-08 13:15:01 +0530

    Add files via upload
    first

---
+++ idea1.js
@@ -0,0 +1,17 @@
+console.log("type the name of fruit you want to buy")
+console.log("type the name of fruit you want to buy");
+
+const y = "toy"
+switch(y) {
+  case "banana":
+    console.log("This is a banana.");  
+    break;
+  case "apple":
+   console.log("This is an apple.");
+    break;
+  case "orange":
+    console.log("This is an orange.");
+      break;
+  default:  
+    console.log("This is not a banana, orange, or apple.");
+}
